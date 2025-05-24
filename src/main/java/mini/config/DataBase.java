7bffde5a06ae333e72dbec66602b64fb32be78a8
chiaquito <chiaki.aws@gmail.com>
2025-05-24 22:49:07 +0900

    modify class loader configuration

--- src/main/java/mini/config/DataBase.java
+++ src/main/java/mini/config/DataBase.java
@@ -9,6 +9,13 @@ public class DataBase {
     private static final HikariDataSource ds;
     
     static {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("MySQL JDBC Driver not found", e);
+        }
+
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl("jdbc:mysql://localhost:3306/db?useSSL=false&serverTimezone=Asia/Tokyo");
         config.setUsername("root");
