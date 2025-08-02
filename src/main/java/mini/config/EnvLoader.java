ca5d2f0625dce568aff037ea84426cca3ed91835
chiaquito <chiaki.aws@gmail.com>
2025-08-02 14:56:53 +0900

    modify health api to study ECS with env variable

--- src/main/java/mini/config/EnvLoader.java
+++ src/main/java/mini/config/EnvLoader.java
@@ -9,11 +9,16 @@ public class EnvLoader {
     static final private HashMap<String, String> env;
 
     static {
+        // env variables for database configuration
         String dbHost = Strings.exists(System.getenv("dbHost"))? System.getenv("dbHost") : "localhost";
         String dbPort = Strings.exists(System.getenv("dbPort"))? System.getenv("dbPort") : "3306";
         String dbName = Strings.exists(System.getenv("dbName"))? System.getenv("dbName") : "db";
         String dbUser = Strings.exists(System.getenv("dbUser"))? System.getenv("dbUser") : "root";
         String dbPassword = Strings.exists(System.getenv("dbPassword"))? System.getenv("dbPassword") : "password";
+
+        // env variables for app behavior
+        String testVariable = Strings.exists(System.getenv("testVariable"))? System.getenv("testVariable") : " :)";
+        
         
         env = new HashMap<>();
         env.put("dbHost", dbHost);
@@ -21,6 +26,8 @@ public class EnvLoader {
         env.put("dbName", dbName);
         env.put("dbUser", dbUser);
         env.put("dbPassword",dbPassword);
+
+        env.put("testVariable", testVariable);
 
         // terraform用のデバッグ // TODO: remove after confirming IaC works
         System.out.printf("dbHost: %s, dbPort: %s, dbName: %s%n", dbHost, dbPort, dbName);
--- src/main/java/mini/controller/HealthController.java
+++ src/main/java/mini/controller/HealthController.java
@@ -5,6 +5,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import mini.config.EnvLoader;
 import mini.controller.response.ErrorResponse;
 
 import javax.ws.rs.Produces;
@@ -21,7 +22,7 @@ public class HealthController {
     @Produces(MediaType.APPLICATION_JSON)
     public Response healthCheck() {
         try {
-            return Response.ok("Application is healthy").build();
+            return Response.ok(String.format("Application is healthy %s", EnvLoader.getEnv().get("testVariable"))).build();
         } catch (RuntimeException e) {
             return ErrorResponse.toResponse(Response.Status.INTERNAL_SERVER_ERROR, e);
         }
